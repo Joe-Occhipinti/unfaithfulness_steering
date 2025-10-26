fffc82bb06f637f4d883ac1756942a9d42599f79
Giovanni Occhipinti <occhidipinti00@gmail.com>
2025-10-26 03:43:25 +0000

    im tired

--- create_train_val_splits.py
+++ create_train_val_splits.py
@@ -18,7 +18,7 @@ from datetime import datetime
 # =============================================================================
 
 INPUT_FILE = "data/sprint4_2025-10-21/annotated/touse_annotated_scie_hist_psy_X_grader_prof_meta_2025-10-25.jsonl"
-OUTPUT_FILE = "data/sprint4_2025-10-21/annotated/touse_annotated_scie_hist_psy_X_grader_prof_meta_psy_2025-10-25.jsonl"
+OUTPUT_FILE = "data/sprint4_2025-10-21/annotated/touse_annotated_scie_hist_psy_X_grader_prof_meta_2025-10-25.jsonl"
 SUMMARY_FILE = "data/sprint4_2025-10-21/summaries/touse_annotated_scie_hist_psy_X_grader_prof_meta_psy_2025-10-25.jsonl"
 TRAIN_RATIO = 0.7
 VAL_RATIO = 0.3
